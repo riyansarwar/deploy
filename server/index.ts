c1739130be21f397fc23539303c166c0ac826800
riyansarwar <m.riyan.sarwar.7@gmail.com>
2025-11-20 21:07:05 +0500

    Fix functions in storage.ts

--- server/index.ts
+++ server/index.ts
@@ -52,7 +52,6 @@ app.use((req, res, next) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
     res.status(status).json({ message });
-    throw err;
   });
 
   // Create HTTP server once here
